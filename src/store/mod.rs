21414f01c13a22e5ca7b401100970f1ba9f9d55f
Greg Yang <gregyang@outlook.com>
2022-02-13 10:23:11 -0800

    Remove Sqlite support

--- src/store/mod.rs
+++ src/store/mod.rs
@@ -5,8 +5,5 @@ pub trait SavePgn {
     fn upsert_pgn(&mut self, name: &str, pgn: &RawPgn) -> SimpleResult<()>;
 }
 
-mod sqlite;
-pub use sqlite::SqliteStore;
-
 mod postgres;
 pub use self::postgres::PostgresStore;
