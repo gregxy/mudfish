c98719e9f5d20ae8855cd4af897e397101bc30a1
Greg Yang <gregyang@outlook.com>
2022-03-10 20:32:30 -0800

    PGN tool

--- src/store/postgres.rs
+++ src/store/postgres.rs
@@ -8,13 +8,11 @@ use crate::pgn::RawPgn;
 
 pub struct PostgresStore {
     client: Client,
-    table: String,
     empty: String,
-    insert_statement: String,
 }
 
 impl PostgresStore {
-    pub fn open<S: Into<String>>(target: &str, table: S) -> SimpleResult<Self> {
+    pub fn open(target: &str) -> SimpleResult<Self> {
         let mut config =
             postgres::config::Config::from_str(target).map_err(|e| simple_error!(e.to_string()))?;
         if config.get_user().is_none() {
@@ -27,92 +25,16 @@ impl PostgresStore {
 
         let mut store = Self {
             client,
-            table: table.into(),
             empty: String::new(),
-            insert_statement: String::new(),
         };
 
         store.create_table()?;
 
-        store.construct_statements();
-
         Ok(store)
     }
 
-    fn construct_statements(&mut self) {
-        self.insert_statement = format!(
-            "INSERT INTO {} (
-    			id,
-    			event,
-    			site,
-    			round,
-    			date,
-    			time,
-    			time_control,
-    			white,
-    			white_title,
-    			white_elo,
-    			white_fide,
-    			black,
-                black_title,
-                black_elo,
-    			black_fide,
-    			eco,
-    			opening,
-    			variation,
-                result,
-                tags,
-    			moves
-    		)
-    		VALUES(
-    			$1,
-    			$2,
-    			$3,
-    			$4,
-    			$5,
-    			$6,
-    			$7,
-    			$8,
-    			$9,
-    			$10,
-    			$11,
-    			$12,
-    			$13,
-    			$14,
-    			$15,
-    			$16,
-                $17,
-                $18,
-                $19,
-                $20,
-                $21)
-            ON CONFLICT (id) DO UPDATE SET
-            	event = $2,
-            	site = $3,
-            	round = $4,
-    			date = $5,
-    			time = $6,
-    			time_control = $7,
-    			white = $8,
-    			white_title = $9,
-    			white_elo = $10,
-    			white_fide = $11,
-    			black = $12,
-                black_title = $13,
-                black_elo = $14,
-    			black_fide = $15,
-    			eco = $16,
-    			opening = $17,
-    			variation = $18,
-                result = $19,
-                tags = $20,
-    			moves = $21",
-            self.table.as_str()
-        );
-    }
-
     fn create_table(&mut self) -> SimpleResult<()> {
-        let statement = format!(
+        let statement =
             "CREATE TABLE IF NOT EXISTS {} (
                     id          VARCHAR(255)    NOT NULL PRIMARY KEY,
                     event       TEXT            DEFAULT '',
@@ -135,24 +57,88 @@ impl PostgresStore {
                     result      VARCHAR(15)     DEFAULT '',
                     tags        TEXT            NOT NULL,
                     moves       TEXT            NOT NULL
-                )",
-            self.table.as_str()
-        );
+                )";
 
         return self
             .client
-            .execute(statement.as_str(), &[])
+            .execute(statement, &[])
             .map(|_| ())
             .map_err(|err| simple_error!(err.to_string()));
     }
 
     pub fn upsert_pgn(&mut self, pgn: &RawPgn) -> SimpleResult<()> {
+        let statement = "INSERT INTO pgn (
+                id,
+                event,
+                site,
+                round,
+                date,
+                time,
+                time_control,
+                white,
+                white_title,
+                white_elo,
+                white_fide,
+                black,
+                black_title,
+                black_elo,
+                black_fide,
+                eco,
+                opening,
+                variation,
+                result,
+                tags,
+                moves
+            )
+            VALUES(
+                $1,
+                $2,
+                $3,
+                $4,
+                $5,
+                $6,
+                $7,
+                $8,
+                $9,
+                $10,
+                $11,
+                $12,
+                $13,
+                $14,
+                $15,
+                $16,
+                $17,
+                $18,
+                $19,
+                $20,
+                $21)
+            ON CONFLICT (id) DO UPDATE SET
+                event = $2,
+                site = $3,
+                round = $4,
+                date = $5,
+                time = $6,
+                time_control = $7,
+                white = $8,
+                white_title = $9,
+                white_elo = $10,
+                white_fide = $11,
+                black = $12,
+                black_title = $13,
+                black_elo = $14,
+                black_fide = $15,
+                eco = $16,
+                opening = $17,
+                variation = $18,
+                result = $19,
+                tags = $20,
+                moves = $21";
+
         return self
             .client
             .execute(
-                self.insert_statement.as_str(),
-                &[
-                    &pgn.id,
+                statement,
+                &[  &pgn.id,
                     pgn.tags.get("Event").unwrap_or(&self.empty),
                     pgn.tags.get("Site").unwrap_or(&self.empty),
                     pgn.tags.get("Round").unwrap_or(&self.empty),
